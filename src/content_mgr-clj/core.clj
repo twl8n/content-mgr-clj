573e98b6c8ccec9ca7475349afc87522ea79c823
Tom Laudeman <tlaudeman@roomkey.com>
2021-01-30 20:15:13 -0500

    Created the state table.

---
+++ src/content_mgr-clj/core.clj
@@ -0,0 +1,132 @@
+(ns machine.core
+  (:require [machine.state :refer :all]
+            [clojure.string :as str]
+            [clojure.pprint :as pp])
+  (:gen-class))
+
+;; Workaround for the namespace changing to "user" after compile and before -main is invoked
+(def true-ns (ns-name *ns*))
+
+;; http://stackoverflow.com/questions/6135764/when-to-use-zipmap-and-when-map-vector
+;; Use (zipmap ...) when you want to directly construct a hashmap from separate sequences of keys and values. The output is a hashmap:
+
+(defn sub-table [edge table]
+  (edge table))
+
+(defn go-again []
+  (print "Go again?")
+  (flush)
+  (let [user-answer (if (= (read-line) "y")
+                      true
+                      false)]
+    (printf "%s\n" user-answer)
+    user-answer))
+
+;; 2021-01-26 Prompt user for any if- functions, but simply run the other functions which should all return false.
+;; If they return false, why do we run them??
+
+;; Functions have two string-ish name formats:
+;; #function[machine.core/if-logged-in]
+;; "machine.core$if_logged_in@5df2f577"
+;; The regex to identify an "if-" function varies depending on type of fn-name.
+;; (re-find #"\$if_" (str fn-name))
+;; (re-find #"/if-" (str fn-name))
+
+(defn user-input [fn-name]
+  (printf "user-input fn-name: %s\n" fn-name)
+  (cond (= fn-name (resolve 'fntrue)) (do (printf "Have fntrue, returning true.\n") (fntrue))
+        (= fn-name (resolve 'fnfalse)) (do (printf "Have fnfalse, returning false.\n" (fnfalse)))
+        (nil? (re-find #"\$if_" (str fn-name))) (fn-name)
+        :else
+        (do
+          (print "Function" (str fn-name) ": ")
+          (flush)
+          (let [user-answer (if (= (read-line) "y")
+                              true
+                              false)]
+            (printf "%s\n" user-answer)
+            user-answer))))
+
+
+;; Loop through tests (nth curr 0) while tests are false, until hitting wait.
+;; Stop looping  if test is true, and change to the next-state-edge (nth curr 2).
+(defn traverse-debug
+  [state]
+  ;; (printf "state=%s\n" state)
+  (if (nil? state)
+    nil
+    (loop [tt (state @machine.state/table)
+           xx 1]
+      (let [curr (first tt)
+            test-result (user-input (nth curr 0))]
+        ;; (printf "curr=%s\n" curr)
+        (cond test-result (if (some? (nth curr 1))
+                              (traverse-debug (nth curr 1))
+                              nil)
+              (seq (rest tt)) (recur (rest tt) (inc xx))
+              :else nil)))))
+
+
+(defn traverse
+  [state]
+  (printf "state=%s\n" state)
+  (if (nil? state)
+    nil
+    (loop [tt (state @machine.state/table)]
+      (let [curr (first tt)
+            test-result ((nth curr 0))]
+        (printf "curr=%s\n" curr)
+        (cond test-result (if (some? (nth curr 1))
+                              (traverse (nth curr 1))
+                              nil)
+              (seq (rest tt)) (recur (rest tt))
+              :else nil)))))
+
+
+(defn demo []
+  (add-state :if-logged-in)
+  (add-state :if-moderator)
+  (add-state :if-want-dashboard)
+  ;; (reset-state)
+  ;; (pp/pprint @machine.state/table)
+  (traverse :login)
+  )
+
+(defn demo2 []
+  (reset-state)
+  (swap! app-state #(merge % {:if-logged-in true}))
+  (println "initial state:" @app-state)
+  (traverse :login)
+  )
+
+(defn demo3 []
+  (reset-state)
+  (swap! app-state #(merge % {:if-logged-in true :if-on-dashboard true}))
+  (println "initial state:" @app-state)
+  (traverse :login))
+
+(defn demo4 []
+  (reset-state)
+  (swap! app-state #(merge % {:if-logged-in true :if-on-dashboard false :if-want-dashboard true :if-moderator true}))
+  (println "initial state:" @app-state)
+  (traverse :login))
+
+(defn demo4-debug []
+  (reset-state)
+  ;; Setting app-state makes no sense in a debug setting. The user will answer all the if- state tests.
+  (loop []
+    (traverse-debug :login)
+    (if (go-again) (recur)
+        nil)))
+
+
+(defn -main
+  "Parse the states.dat file."
+  [& args]
+  (printf "args: %s\n" args)
+  ;; Workaround for the namespace changing to "user" after compile and before -main is invoked
+  (in-ns true-ns)
+  
+  (let [first-arg (nth args 0)]
+    (if (= first-arg "debug") (traverse-debug :page_search)
+        (traverse :page_search))))
---
+++ src/content_mgr-clj/state.clj
@@ -0,0 +1,179 @@
+(ns machine.state
+  (:require [clojure.string :as str]
+            [clojure.pprint :as pp]))
+
+(defn msg [arg] (printf "%s\n" arg))
+
+(def app-state (atom {}))
+
+(defn reset-state [] 
+  (swap! app-state (fn [foo] {})))
+
+(defn add-state [new-kw]
+  (swap! app-state #(apply assoc % [new-kw true])))
+
+(defn is-jump? [arg] false)
+
+(defn is-wait?
+  "(str (type arg)) is something like class machine.core$wait"
+  [arg] (= "$wait" (re-find #"\$wait" (str (type arg)))))
+
+(defn is-return? [arg] false)
+(defn jump-to [arg jstack] [arg (cons arg jstack)])
+
+(defn if-logged-in [] (let [rval (@app-state :if-logged-in)] (msg (str "running if-logged-in: " rval)) rval))
+(defn if-moderator [] (let [rval (@app-state :if-moderator)] (msg (str "running if-moderator: " rval)) rval))
+(defn if-on-dashboard [] (let [rval (@app-state :if-on-dashboard)] (msg (str "if-on-dashboard: " rval)) rval))
+(defn if-want-dashboard [] (let [rval (@app-state :if-want-dashboard)] (msg (str "if-want-dashboard: " rval)) rval))
+
+(defn draw-login [] (msg "running draw-login") false)
+(defn force-logout [] (msg "forcing logout") (swap! app-state #(apply dissoc %  [:if-logged-in])) false)
+(defn draw-dashboard-moderator [] (add-state :if-on-dashboard)  (msg "running draw-dashboard-moderator") false)
+
+(defn draw-dashboard [] (msg "running draw-dashboard") false)
+
+(defn logout [] (msg "running logout") false)
+(defn login [] (msg "running login") false)
+(defn fntrue [] (msg "running fntrue") true)
+(defn fnfalse [] (msg "running fnfalse") false)
+(defn wait [] (msg "running wait, returning false") true) ;; return true because wait ends looping over tests
+(defn noop [] (printf "running noop\n"))
+
+
+;; default is page_search
+
+(def table
+  (atom
+   {:page_search
+    [[if-edit :edit_page]
+     [if-delete :ask_delete_page]
+     [if-insert :edit_new_page]
+     [if-item :item_search]
+     [if-site_gen :site_gen]
+     [page_search nil]]
+    ;; 0	page_search	$edit	  null()	  edit_page
+    ;; 1	page_search	$delete	  null()	  ask_delete_page
+    ;; 2	page_search	$insert	  null()          edit_new_page
+    ;; 3	page_search	$item	  null()	  item_search
+    ;; 4	page_search	$site_gen site_gen()	  next
+    ;; 5	page_search	$true	  page_search()	  wait
+
+    :site_gen
+    [[site_gen nil]
+     [fntrue :page_search]]
+
+    :ask_del_page
+    [[if-confirm :delete_page]
+     [fn-true :page_search]]
+    ;; 0	ask_del_page	$confirm  delete_page()	  page_search
+    ;; 1	ask_del_page	$true	  ask_del_page()  wait
+
+    :delete_page
+    [[delete_page :page_search]]
+
+    :edit_page
+    [[if-save :save_page]
+     [if-continue :save_page_continue]
+     [if-next :save_next]
+     [edit_page nil]]
+    ;; 0	edit_page	$save	  save_page()	  page_search
+    ;; 1	edit_page	$continue save_page()	  next
+    ;; 2	edit_page	$next	  save_page()	  next
+    ;; 3	edit_page	$next	  next_page()	  next
+    ;; 4	edit_page	$true	  edit_page()	  wait
+
+    :save_page
+    [[save_page :page_search]]
+
+    :save_page_continue
+    [[save_page nil]
+     [edit_page :edit_page]]
+
+    :save_next
+    [[save_page nil]
+     [next_page :edit_page]]
+
+    :edit_new_page
+    [[if-save :insert_page]
+     [if-continue :insert_continue]
+     [edit_new_page nil]]
+    ;; 0	edit_new_page	$save	  insert_page()	  page_search
+    ;; 1	edit_new_page	$continue insert_page()	  next
+    ;; 2	edit_new_page	$continue clear_cont()	  edit_page
+    ;; 3	edit_new_page	$true	  edit_new_page() wait
+    
+    :insert_page
+    [[insert_page :page_search]]
+    :insert_continue
+    [[insert_page nil]
+     [clear_cont :edit_page]]
+
+    :item_search
+    [[if-edit :edit_item]
+     [if-page-gen :page_gen]
+     [if-auto-gen :auto_gen]]
+    ;; 0	item_search	$edit	  null()	  edit_item
+    ;; 1	item_search	$page_gen page_gen()	  next
+    ;; 2	item_search	$auto_gen auto_gen()	  next
+    ;; 3	item_search	$true	  item_search()	  wait
+
+    :page_gen
+    [[page_gen :item_search]]
+    :auto_gen
+    [[auto_gen :item_search]]
+
+    :edit_item
+    [[if-save :save_item]
+     [if-continue :save_item_continue]
+     [save_item nil]
+     [next_item nil]
+     [if-con-pk edit_con_pk]
+     [fn-true :item_searchs]]
+    ;; 0	edit_item	$save	  save_item()	  item_search
+    ;; 1	edit_item	$continue save_item()	  next
+    ;; 2	edit_item	$continue edit_item()	  wait
+    ;; 3	edit_item	$next	  save_item()	  next
+    ;; 4	edit_item	$next	  next_item()	  next
+    ;; 5    edit_item       $con_pk	  edit_item()	  wait aka edit_con_pk
+    ;; 6 	edit_item	$true 	  null()	  item_search
+
+    :save_item
+    [[save_item :item_search]]
+    :save_item_continue
+    [[save_item nil]
+     [edit_item nil]]
+    :edit_con_pk
+    [[edit_item :item_search]]
+    }))
+
+
+;; {:state-edge [[test-or-func next-state-edge] ...]}
+(def orig-table
+  (atom
+   {:login
+    [[if-logged-in :pages]
+     [force-logout nil]
+     [draw-login nil]
+     [wait nil]]
+    
+    :login-input
+    [[if-logged-in :dashboard]
+     [login :login]]
+
+    :pages
+    [[if-on-dashboard :dashboard-input]
+     [if-want-dashboard :dashboard]
+     [wait nil]]
+
+    :dashboard
+    [[if-moderator :dashboard-moderator]
+     [draw-dashboard]
+     [wait nil]]
+
+    :dashboard-moderator
+    [[draw-dashboard-moderator :dashboard-input]]
+
+    :dashboard-input
+    [[wait nil]]
+    }))
+
